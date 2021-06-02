e39d91b46d54b76d0c843cdb5ccb23eb728e92c9
DESKTOP-SCM0CPU\PC-22 <barsha.s@vitasoft-tech.com>
2021-06-02 22:53:59 +0530

    scss change

--- Barsha/src/app/pages/post-card/post-card.component.ts
+++ Barsha/src/app/pages/post-card/post-card.component.ts
@@ -7,17 +7,22 @@ import { Component, OnInit , Input  } from '@angular/core';
 })
 export class PostCardComponent implements OnInit {
   @Input() name : string;
+  likes:number;
+  dislikes:number;
   ishidden: Boolean =true ;
   show(): void{
-   
-   
       this.ishidden=false;
-    
-   
-    
-  }
+    }
+      like():number{
+        return this.likes+=1;
+    }
+    dislike():number{
+      return this.dislikes +=1;
+    }
   constructor() {
     this.name=''
+    this.likes=0;
+    this.dislikes=0;
    }
 
   ngOnInit(): void {
