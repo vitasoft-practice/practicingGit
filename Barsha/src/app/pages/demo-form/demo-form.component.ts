3b6898d9719b5d0c869dda2f0162ee2f3312ae9f
DESKTOP-SCM0CPU\PC-22 <barsha.s@vitasoft-tech.com>
2021-06-09 01:07:01 +0530

    "form builder "

--- Barsha/src/app/pages/demo-form/demo-form.component.ts
+++ Barsha/src/app/pages/demo-form/demo-form.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup , FormBuilder, Form } from '@angular/forms';
 
 
 interface formdata {
   name:string;
   sub:string;
+  street:string;
+  city:string;
+  state:string;
+  zip:number;
 }
 
 @Component({
@@ -16,10 +20,28 @@ export class DemoFormComponent implements OnInit {
  
   formData:formdata[] =[];
    
-  profileForm = new FormGroup({
-    name : new FormControl(''),
-    sub : new FormControl(''),
+  // profileForm = new FormGroup({
+  //   name : new FormControl(''),
+  //   sub : new FormControl(''),
+    
+  //   address: new FormGroup({
+  //     street: new FormControl(''),
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     zip: new FormControl('')
+  //   })
+  // });
+  profileForm = this.fb.group({
+    name: [''],
+    sub: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
   });
+
   
   ishidden: Boolean ;
   show(): void{
@@ -28,7 +50,7 @@ export class DemoFormComponent implements OnInit {
     this.ishidden=false;
     
   }
-  constructor() { 
+  constructor(private fb:FormBuilder) { 
     this.ishidden = true;
     // this.formData=[];
   }
@@ -39,7 +61,7 @@ export class DemoFormComponent implements OnInit {
  onSubmit(){
   //  this.formData.push(this.profileForm.value);
   this.formData.push(this.profileForm.value);
-   console.log(this.formData);
+   console.log(this.profileForm.value.address);
    
  }
 
