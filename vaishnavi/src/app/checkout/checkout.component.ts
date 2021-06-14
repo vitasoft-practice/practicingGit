52c11c3c7a004776a80bd0ad9a284f5992365196
DESKTOP-1UUL9S4\pc-18 <vaishnavi.m@vitasoft-tech.com>
2021-06-15 05:01:24 +0530

    validation in reactive form

--- vaishnavi/src/app/checkout/checkout.component.ts
+++ vaishnavi/src/app/checkout/checkout.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl,FormBuilder} from '@angular/forms';
-// import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl,FormBuilder, Validators, AbstractControl} from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -8,38 +7,76 @@ import { FormGroup, FormControl,FormBuilder} from '@angular/forms';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
+  
 
-  // checkoutForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) {}
+//   checkoutForm: FormGroup;
+// constructor(private formBuilder: FormBuilder){
+    
+//   this.checkoutForm = this.formBuilder.group({
+//     name: ["", Validators.required],
+//     email: ["", [Validators.required,Validators.email]],
+//     check: ["", Validators.requiredTrue]
+//   });
+// }
 
-  checkouForm2 = new FormGroup({
-    name: new FormControl(''),
-    email:new FormControl(''),
-    check: new FormControl('')
-  })
 
-   
+    checkoutForm = new FormGroup({
+    name: new FormControl("",Validators.required),
+    email: new FormControl("",Validators.required),
+    check: new FormControl("",Validators.requiredTrue)
+  });
+    
 
+
+  get email(){return this.checkoutForm.get('email')};
+    // imp
+    // checkoutForm: FormGroup;
+    // constructor(){
+    //   this.checkoutForm = new FormGroup({
+    //     name: new FormControl("",Validators.required),
+    //     email: new FormControl("",Validators.required),
+    //     check: new FormControl("",Validators.requiredTrue)
+    //   });
+    //     }
+
+
+
+
+  // checkoutForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   email:new FormControl(''),
+  //   check: new FormControl('')
+  // })
+// elem =false ;
+//   email(){
+//     this.elem = checkoutForm.value.email;
+//   }
    postData(){
-     console.log(this.checkouForm2);
-     console.log(this.checkouForm2.value);
+     console.log(this.checkoutForm);
+     console.log(this.checkoutForm.value);
      
    };
+   
+   
 
   ngOnInit(): void {
 
-  //   this.checkoutForm = this.formBuilder.group({
-  //   name:[''],
-  //   email:[''],
-  //   check: ['']
-  // });
+
+    
 
 
     // this.checkoutForm = new FormGroup({
-    //   name: new FormControl(""),
-    //   email: new FormControl(""),
-    //   check: new FormControl("")
-    // });
-  }
+    //   name: new FormControl(this.hero.name,[
+    //     Validators.required,
+    //     Validators.minLength(4)
+    //   ])
+        
+    //   });
 
+
+
+    // testinggg
+
+    
+  }
 }
