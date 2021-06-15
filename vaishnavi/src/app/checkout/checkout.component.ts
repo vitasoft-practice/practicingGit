a130031dc80b3fd9eab3b82b3e7a78b2d5def50a
DESKTOP-1UUL9S4\pc-18 <vaishnavi.m@vitasoft-tech.com>
2021-06-16 04:51:58 +0530

    1.formGroup-setValues,reset form,valueChanges,statusChanges. 2.Form Array-nested Form

--- vaishnavi/src/app/checkout/checkout.component.ts
+++ vaishnavi/src/app/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl,FormBuilder, Validators, AbstractControl} from '@angular/forms';
+import { FormGroup, FormControl,FormBuilder, Validators, AbstractControl, FormArray} from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -8,27 +8,42 @@ import { FormGroup, FormControl,FormBuilder, Validators, AbstractControl} from '
 })
 export class CheckoutComponent implements OnInit {
   
-
-//   checkoutForm: FormGroup;
-// constructor(private formBuilder: FormBuilder){
+valueCapture ="";
+  checkoutForm: FormGroup;
+constructor(private formBuilder: FormBuilder){
     
-//   this.checkoutForm = this.formBuilder.group({
-//     name: ["", Validators.required],
-//     email: ["", [Validators.required,Validators.email]],
-//     check: ["", Validators.requiredTrue]
-//   });
-// }
+  this.checkoutForm = this.formBuilder.group({
+    name: ["", Validators.required],
+    email: ["", [Validators.required,Validators.email]],
+    check: ["", Validators.requiredTrue],
 
 
-    checkoutForm = new FormGroup({
-    name: new FormControl("",Validators.required),
-    email: new FormControl("",Validators.required),
-    check: new FormControl("",Validators.requiredTrue)
+    items: this.formBuilder.array([
+    //   new FormControl('a'),
+    //   new FormControl('a'),
+    //   new FormControl('a')
+
+      this.formBuilder.group({
+        id1:['1'],
+        id2:['2'],
+        id3:['3']
+      })
+
+    ])
   });
+}
+
+
+
+  //   checkoutForm = new FormGroup({
+  //   name: new FormControl("",Validators.required),
+  //   email: new FormControl("",[Validators.required,Validators.email]),
+  //   check: new FormControl("",Validators.requiredTrue)
+  // });
     
 
 
-  get email(){return this.checkoutForm.get('email')};
+  
     // imp
     // checkoutForm: FormGroup;
     // constructor(){
@@ -54,29 +69,60 @@ export class CheckoutComponent implements OnInit {
    postData(){
      console.log(this.checkoutForm);
      console.log(this.checkoutForm.value);
-     
+     this.resetForm();
    };
    
-   
+   resetForm(){
+     this.checkoutForm.reset();
+   };
 
   ngOnInit(): void {
+// ------------------------ valueChanges() --------------////
+    this.checkoutForm.get('name')?.valueChanges.subscribe(data =>{
+      this.valueCapture = data;
+      
+    })
+    // this.checkoutForm.valueChanges.subscribe(datas =>{
+    //   console.log(datas);
+    // })
 
-
+ 
+    console.log(this.checkoutForm.get('items')?.value);
     
 
+// -------------------------statusChange () Vitasoft@2021  -----------------//
+// this.checkoutForm.statusChanges.subscribe(datas =>{
+//   console.log(datas);
+// })
+// //////////// ------------ setValue() ----------- //////////
+//  this.checkoutForm.setValue({
+//    name:'Miss',
+//    email:'@gmail',
+//    check:'true'
+//  })
+// //////////// ------------ patchValue() ----------- //////////    
+// this.checkoutForm.patchValue({
+//   name:'Mr/Miss',
+//   email:'123@gmail.com'
+// });
 
-    // this.checkoutForm = new FormGroup({
-    //   name: new FormControl(this.hero.name,[
-    //     Validators.required,
-    //     Validators.minLength(4)
-    //   ])
-        
-    //   });
-
-
-
-    // testinggg
-
-    
   }
+
+
+  get items(){
+    return this.checkoutForm.get('items') as FormArray;
+  }
+  addnew(){
+    const itemLength = this.items.length;
+    const newItem = this.formBuilder.group({
+
+      
+        id1:['2'],
+        id2:[''],
+        id3:['']
+ 
+    });
+    this.items.push(newItem);
+  }
+
 }
