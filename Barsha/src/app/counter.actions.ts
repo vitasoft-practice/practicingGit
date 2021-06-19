c910307c56f5e7518f9f8583580cdfcf7baf8d4c
DESKTOP-SCM0CPU\PC-22 <barsha.s@vitasoft-tech.com>
2021-06-20 00:03:45 +0530

    "directory changes II "

--- Barsha/src/app/counter.actions.ts
+++
@@ -1,6 +0,0 @@
-import { createAction,props } from "@ngrx/store";
-import { User } from "./user";
-export const signup = createAction('[Signup Page], Signup',
-                        props<{user: User}>());
-
-// const newSignupAction = signup({user:}); 
--- Barsha/src/app/user.actions.spec.ts
+++
@@ -1,7 +0,0 @@
-import * as UserActions from './user.actions';
-
-describe('User', () => {
-  it('should create an instance', () => {
-    expect(new UserActions.LoadUsers()).toBeTruthy();
-  });
-});
