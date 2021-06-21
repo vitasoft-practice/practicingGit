8c7a888980d9d80aa84a471bd54e22e775b593d9
DESKTOP-4VHA75N\pc-27 <andrew44knight@gmail.com>
2021-06-22 02:07:19 +0530

    Input fields2

--- Shared/pages/index.tsx
+++ Shared/pages/index.tsx
@@ -1,18 +1,42 @@
 import style from "styled-components";
 import BasicLayout from "../layout/Basic";
 import Input from "../components/Input";
-const Title = style.h1`
-  color: red;
-`;
+import { InputPropsInterface } from "../interfaces/inputInterface";
+import { useContext, useState } from "react";
+import { InputContext, InputProvider } from "../contexts/InputContext";
+
+
+
 
 const Home = () => {
+
+  const {borderColortext, setBorderColortext , inputVal, setInputVal,setPlaceHolderText , placeholderText} = useContext(InputContext);
+  const green = "#4AA96C";
+  const red = "#F54748";
+  console.log(placeholderText,inputVal,borderColortext);
+  const handleClick = () =>{
+    if(inputVal!="admin"){
+      setBorderColortext(red);
+      setInputVal("");
+      setPlaceHolderText("Invalid Input");
+    }else{
+      setBorderColortext(green);
+    }
+  }
+
+  const inputProps: InputPropsInterface = {
+    borderColor: borderColortext,
+    value: inputVal,
+    type: "text",
+    placeholder: placeholderText,
+  }
+
   return (
     <BasicLayout>
-        <div>
-          <p>hello</p>
-          <Title>Title</Title>
-          <Input />
-        </div>
+      <InputProvider>
+          <Input props={inputProps} />
+          <button onClick={handleClick}>Check</button>
+      </InputProvider>  
     </BasicLayout>
    
   );
