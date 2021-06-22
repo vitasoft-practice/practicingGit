c73504c4c013a826b3606a4a0750049581ce493b
DESKTOP-SCM0CPU\PC-22 <barsha.s@vitasoft-tech.com>
2021-06-22 05:39:40 +0530

    z-suite design scss

---
+++ Barsha/z-suite-demo/src/app/components/group1/group1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-group1',
+  templateUrl: './group1.component.html',
+  styleUrls: ['./group1.component.scss']
+})
+export class Group1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
