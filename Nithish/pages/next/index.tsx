c27e3f78f72c4cbbde9039f1cdecbe147f75f676
DESKTOP-4VHA75N\pc-27 <andrew44knight@gmail.com>
2021-06-23 05:36:16 +0530

    Understanding frontend frameworks part 2 -  Final commit

--- Nithish/pages/next/index.tsx
+++ Nithish/pages/next/index.tsx
@@ -1,23 +1,31 @@
 import Input from "../../components/Input";
 import form from "../../styles/form.module.scss";
 import Form from '../../components/Form';
-import {useEffect, useState} from 'react';
-import {FormProvider} from "../../contexts/FormContext";
+import { useEffect, useState } from 'react';
+import { FormProvider } from "../../contexts/FormContext";
 import { useRouter } from "next/router";
 import { getRouteMatcher } from "next/dist/next-server/lib/router/utils";
 import { applyMiddleware, createStore } from "redux";
-import LoginReducers from "../../redux/reducers/login";
+// import LoginReducers from "../../redux/reducers/login";
 import { Provider } from "react-redux";
 import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import Home from "./Home";
+import ReduxForms from "./reduxForm";
+import { submit } from "redux-form";
+import { reducers } from "../../redux/actions";
+import LoginReducers from "../../redux/reducers/login";
 
 const Next = () => {
+
     const router = useRouter();
     const composeEnhancers = composeWithDevTools(applyMiddleware(thunkMiddleware));
-    const store = createStore(LoginReducers, composeEnhancers);
-    return(
-       <Provider store={store}>
+    const store = createStore(reducers, composeEnhancers);
+    const submit = (val: any) => {
+        console.log(val);
+    }
+    return (
+        <Provider store={store}>
             <div className={form.formcontainer}>
                 <FormProvider>
                     <Form />
@@ -40,8 +48,9 @@ const Next = () => {
                         </ul>
                     </div>
                 </div>
+                <ReduxForms onSubmit={submit} />
             </div>
-       </Provider>
+        </Provider>
     )
 }
 export default Next;
