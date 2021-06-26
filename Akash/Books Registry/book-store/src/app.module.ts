fe0aa4c444922f55c220dc88a78dbf4bf0c0cf9f
DESKTOP-ND06BFD\pc-29 <akash.t@vitasoft-tech.com>
2021-06-26 05:49:11 +0530

    Revert "Revert "Creating Database relation""
    This reverts commit 98d24585f86c41cba0dc79fd56b502e9dfbceded.

--- Akash/Books Registry/book-store/src/app.module.ts
+++ Akash/Books Registry/book-store/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BooksModule } from './books/books.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [BooksModule,MongooseModule.forRoot('mongodb://127.0.0.1:27017/Book-store')]
+  imports: [BooksModule,MongooseModule.forRoot('mongodb://127.0.0.1:27017/Book-store'), UsersModule]
 })
 export class AppModule {}
--- Akash/Books Registry/book-store/src/books/entities/book.entity.ts
+++ Akash/Books Registry/book-store/src/books/entities/book.entity.ts
@@ -1,13 +1,13 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { ApiProperty } from "@nestjs/swagger";
 import { Document } from "mongoose";
+import * as mongoose from 'mongoose';
+import { User } from "src/users/entities/user.entity";
 
 export type BookDocument = Book & Document;
 
 @Schema()
 export class Book {
-
-    
 
     @ApiProperty()
     @Prop()
@@ -17,8 +17,9 @@ export class Book {
     @Prop()
     Price: number;
 
-    
-
+    @ApiProperty()
+    @Prop({type : mongoose.Schema.Types.ObjectId, ref : 'User'})
+    User : User;
     
 }
 
