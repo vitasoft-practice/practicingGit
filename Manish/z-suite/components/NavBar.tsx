656076f845102401d2b66542248ffa12c7c12204
Manish Kumar <manish.k@vitasoft-tech.com>
2021-07-09 23:13:15 +0530

    initial commit

---
+++ Manish/z-suite/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/NavBar.module.scss'
+
+const NavBar = () => {
+    return (
+        <div className={styles.nav}>
+            hello
+        </div>
+    )
+}
+
+export default NavBar
