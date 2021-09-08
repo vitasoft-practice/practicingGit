d920280a82c0c25a0f2247627b50b293bebab665
BarshaSingha <85198847+BarshaSingha@users.noreply.github.com>
2021-09-09 00:09:40 +0530

    react intl-tel-input with bitdev

--- Barsha/reactBIt/scope/ui/form/form.tsx
+++ Barsha/reactBIt/scope/ui/form/form.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from "./form.module.scss";
 import { PhoneInput } from "@company/scope.ui.phone-input";
-
+import IntlTelInput from 'react-intl-tel-input';
+import 'react-intl-tel-input/dist/main.css';
 import { TextField, Select, MenuItem } from "@material-ui/core";
-import IntlTelInput from "react-intl-tel-input";
+// import IntlTelInput from "react-intl-tel-input";
 
 export type FormProps = {
   /**
@@ -17,6 +18,11 @@ export function Form({ text }: FormProps) {
     <div className={styles.form}>
       <form>
         <PhoneInput text="contact" />
+
+        <IntlTelInput
+  containerClassName="intl-tel-input"
+  inputClassName="form-control"
+/>
       </form>
       <br />
     </div>
