d3cfa3070bd4f1f1d3771d00103a3e9e9f8c1071
unknown <kamalesh14998@gmail.com>
2022-11-24 16:32:59 +0530

    demo page plus some animation

--- Kamalesh/wellProz/task1/src/components/CareExpPage/careExpPage.tsx
+++ Kamalesh/wellProz/task1/src/components/CareExpPage/careExpPage.tsx
@@ -9,32 +9,40 @@ const CareExpPAge = () => {
     const [scrolledFirst,setscrolledFirst]= useState(false);
     const [scrolledSec,setscrolledSec]= useState(false);
     const [scrolledThird,setscrolledThird]= useState(false);
+    const [scrolled,setscrolled]= useState(false);
+    const [scrolledType,setscrolledType]= useState('');
     var firstImage = require('./careFirstScroll.png')
     var secondImage =  require('./careSecond.png')
     var thirdImage =  require('./carePicture.png')
     const [imageSrc,setimageSrc]= useState(firstImage);
-    function SignUpAuth(){
-        alert("Hiii")
-    }
     function scrollPicture(){
         console.log("//",window.scrollY)
         if(window.scrollY >= 1100 && window.scrollY <=1200){
             setscrolledFirst(true)
             setscrolledSec(false)
             setscrolledThird(false)
+            setscrolled(true)
+            setscrolledType('firstImage')
             setimageSrc(firstImage)
         }
         else if(window.scrollY >= 1200 && window.scrollY <=1300){
             setscrolledFirst(false)
             setscrolledSec(true)
             setscrolledThird(false)
+            setscrolled(true)
+            setscrolledType('secondImage')
             setimageSrc(secondImage)
         }
         else if(window.scrollY >= 1300){
             setscrolledFirst(false)
             setscrolledSec(false)
             setscrolledThird(true)
+            setscrolled(true)
+            setscrolledType('thirdImage')
             setimageSrc(thirdImage)
+        }
+        else{
+            setscrolled(false)
         }
     }
     window.addEventListener('scroll',scrollPicture)
@@ -42,15 +50,15 @@ const CareExpPAge = () => {
         <div className="CareExpPAge">
             <div className="careExpwrapper">
                 <div className="LeftExp">
-                    <div className="imageclass">
-                        <img src= { imageSrc } className="careImageClass" alt="" />
+                    <div className={scrolled ? "imageclass fadeactive " + scrolledType : "fadeinactive"}>
+                        <img src= { imageSrc }  className="careImageClass" alt="" />
                     </div>
                 </div>
                 <div className="RightExp">
                     <div className="mainHeader">Create a care experience your patients love </div>
                     <div className="expContent">For the first time ever, you can now offer clinical lab testing, practitioner grade supplements, your own services and more with shipping, fulfillment, and logistics all done-for-you in a single platform. </div>
                     <div className={scrolledFirst ? "firstOption active" : "inactive"}>
-                        <div className="secondHeader" onClick={SignUpAuth} ><span><img src= { bulletTick } className="bulletin" alt="" /></span> Precision Wellness Testing </div>
+                        <div className="secondHeader"><span><img src= { bulletTick } className="bulletin" alt="" /></span> Precision Wellness Testing </div>
                         <div className="expContent">Help your patients get to the root cause of their health issue by offering a vast menu of specialty wellness lab testing. Easily integrate custom Vibrant America panels into your store for a truly personalized experience.  </div>
                     </div>
                     <div className={scrolledSec ? "secondOption active" : "inactive"}>
--- Kamalesh/wellProz/task1/src/components/demoPage/demoPage.tsx
+++ Kamalesh/wellProz/task1/src/components/demoPage/demoPage.tsx
@@ -5,11 +5,34 @@ const DemoPage = () => {
     return (
         <div className="DemoPage">
             <div className="demoWrapper">
-                <div className="demoForm">
-
-                </div>
-                <div className="demoInfo">
-                    
+                <div className="topParent">
+                    <div className="subParent">
+                    <div className="demoForm">
+                        <div className="demoFormWrapper">
+                            <div className="demoFormFields">
+                                <div className="demoHeader">
+                                Request a Demo
+                                </div>
+                                <div className="demoInputFields">
+                                    <div className="demoUserInput"><input placeholder="First Name" type="text" /></div>
+                                    <div className="demoUserInput"><input placeholder="Last Name" type="text" /></div>
+                                    <div className="demoUserInput"><input placeholder="Email Address" type="text" /></div>
+                                    <div className="demoUserInput"><input placeholder="Pactice Name" type="text" /></div>
+                                    <div className="demoUserInput"><input placeholder="Contact Number" type="text" /></div>
+                                    <div className="demoUserInput"><input placeholder="How did you hear about us" type="text" /></div>
+                                </div>
+                                <div className="demoRequest">Send Request</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="demoInfo">
+                        <div className="demoInfowrapper">
+                            <div className="demoInfoHeader">Ready for a stress-free way to help your practice thrive?</div>
+                            <div className="demoInfoContent"><span style={{textDecoration: 'underline'}}>Increase visits</span>, <span style={{textDecoration: 'underline'}}>wow patients</span>, and <span style={{textDecoration: 'underline'}}>save staff time</span> with <span className="WellProzSpan">WellProZ.</span></div>
+                            <div className="demoButton">Try it For Free</div>
+                        </div>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
