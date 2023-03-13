fc155393f66c8914e2e12fa761dda25e87067899
nit <nithish.k@vitasoft-tech.com>
2023-03-13 14:00:26 +0530

    feat: rmq implementation of microservices

---
+++ Nithish/microservices/microservice-client/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'Practice',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://guest:guest@localhost:5672/Hello'],
+          queue: 'user-messages',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ Nithish/microservices/microservices-app/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://guest:guest@localhost:5672/Hello'],
+      queue: 'user-messages',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+  app.listen().then(() => {
+    console.log('Microservice is listening');
+  });
+}
+bootstrap();
