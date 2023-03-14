16b01f571ee482ea2980305e745d7f3bc2d5dc86
nit <nithish.k@vitasoft-tech.com>
2023-03-14 12:14:50 +0530

    feat: grpc implementation of microservices

---
+++ Nithish/microservices-grpc/grpc-microservices-client/src/grpc.interface.ts
@@ -0,0 +1,14 @@
+import { Observable } from 'rxjs';
+
+export interface RemoveRequest {
+  name: string;
+}
+
+export interface Movie {
+  name: string;
+  rating: number;
+}
+export interface MovieService {
+  addMovie(movie: Movie): Observable<Movie[]>;
+  removeMovie(nameOfMovie: string): Observable<Movie[]>;
+}
