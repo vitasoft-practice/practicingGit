c5c8cc17662df384102467243c76f6dbff888100
Vaishnavi-Manogaran <85192082+Vaishnavi-Manogaran@users.noreply.github.com>
2023-04-12 08:38:33 +0530

    removing unwanted file

--- vaishnavi/client/dist/app.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { ClientKafka } from '@nestjs/microservices/client';
-export declare class AppController {
-    private readonly client;
-    constructor(client: ClientKafka);
-    onApplicationBootstrap(): Promise<void>;
-    create(): import("rxjs").Observable<any>;
-    create2(): import("rxjs").Observable<any>;
-}
